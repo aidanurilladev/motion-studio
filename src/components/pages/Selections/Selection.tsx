3c21b784ceed07cb2046c02811b90c81693414f9
ayat kalybekova <kalybekAyat@outlook.com>
2024-11-06 21:52:02 +0600

    dropdown

--- src/components/pages/Selections/Selection.tsx
+++ src/components/pages/Selections/Selection.tsx
@@ -1,25 +1,41 @@
+import { useEffect, useState } from "react";
 import scss from "./selection.module.scss";
+
 const Selection = () => {
+  const selections = [
+    {
+      options: [
+        "Higher Education",
+        "Secondary Education",
+        "Higher Education",
+        "Language courses for youth",
+        "Children’s language camps",
+      ],
+    },
+    {
+      options: [
+        "Specialization",
+        "IT Technologies",
+        "Business",
+        "Medicine",
+        "International Relationships",
+        "Tourism",
+      ],
+    },
+    {
+      options: ["Countries", "America", "Germany", "Indonesia", "South Korea"],
+    },
+  ];
+
   return (
     <div className={scss.filters}>
-      <select>
-        <option value="">Higher Education</option>
-        <option value="">Higher Education</option>
-        <option value="">Higher Education</option>
-        <option value="">Higher Education</option>
-      </select>
-      <select>
-        <option value="">specialization</option>
-        <option value="">Higher Education</option>
-        <option value="">Higher Education</option>
-        <option value="">Higher Education</option>
-      </select>
-      <select>
-        <option value="">Сountries</option>
-        <option value="">Higher Education</option>
-        <option value="">Higher Education</option>
-        <option value="">Higher Education</option>
-      </select>
+      {selections.map((el, index) => (
+        <select key={index}>
+          {el.options.map((elem, index) => (
+            <option key={index}>{elem}</option>
+          ))}
+        </select>
+      ))}
     </div>
   );
 };
