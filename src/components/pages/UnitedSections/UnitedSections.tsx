1cef23a0521fbd2dca4101f45fa35d5d83b1c84b
ayat kalybekova <kalybekAyat@outlook.com>
2024-11-07 18:36:50 +0600

    countries-adaptation

--- src/components/pages/UnitedSections/UnitedSections.tsx
+++ src/components/pages/UnitedSections/UnitedSections.tsx
@@ -5,6 +5,58 @@ import SolbrigeUniversity from "@/assets/img/SolbrigeUniversity.png";
 import { IoLocationOutline } from "react-icons/io5";
 
 const UnitedSections = () => {
+  const universities = [
+    {
+      id: 1,
+      name: "Solbridge University",
+      location: "Perth",
+      age: "From 16",
+      imageUrl: "URL_изображения_1",
+    },
+    {
+      id: 2,
+      name: "La Trobe University",
+      location: "Riga",
+      age: "From 16",
+      imageUrl: "URL_изображения_2",
+    },
+    {
+      id: 3,
+      name: "TSI University",
+      location: "Riga",
+      age: "From 16",
+      imageUrl: "URL_изображения_3",
+    },
+    {
+      id: 4,
+      name: "Solbridge University",
+      location: "Perth",
+      age: "From 16",
+      imageUrl: "URL_изображения_4",
+    },
+    {
+      id: 5,
+      name: "Edith University",
+      location: "Melbourne",
+      age: "From 16",
+      imageUrl: "URL_изображения_5",
+    },
+    {
+      id: 6,
+      name: "DCU University",
+      location: "Riga",
+      age: "From 16",
+      imageUrl: "URL_изображения_6",
+    },
+    {
+      id: 7,
+      name: "Cowan University",
+      location: "Boston",
+      age: "From 16",
+      imageUrl: "URL_изображения_7",
+    },
+  ];
+
   return (
     <section className={scss.UnitedSections}>
       <div className="container">
@@ -13,33 +65,32 @@ const UnitedSections = () => {
           <Selection />
 
           <div className={scss.universities}>
-            <div className={scss.university}>
-              <div className={scss.card}>
+            {universities.map((university) => (
+              <div key={university.id} className={scss.card}>
                 <Image
                   src={SolbrigeUniversity}
                   alt="university"
                   width={230}
                   height={160}
                 />
-
                 <div className={scss.info}>
                   <div className={scss.name}>
                     <h6>Name:</h6>
-                    <h3>Solbrige University</h3>
+                    <h3>{university.name}</h3>
                   </div>
                   <div className={scss.location}>
                     <h6>
                       Location <IoLocationOutline />
                     </h6>
-                    <h3>Perth</h3>
+                    <h3>{university.location}</h3>
                   </div>
-                  <div className={scss.location}>
+                  <div className={scss.age}>
                     <h6>Age:</h6>
-                    <h3>From 16</h3>
+                    <h3>{university.age}</h3>
                   </div>
                 </div>
               </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
