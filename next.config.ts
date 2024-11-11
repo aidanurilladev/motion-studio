d7c7e9998a866177ebd2e7c0719d1c7fadd88d77
aidanurilladev <aidanurillaa@gmail.com>
2024-11-11 10:24:49 +0600

    updating

--- next.config.ts
+++ next.config.ts
@@ -2,29 +2,32 @@ module.exports = {
   i18n: {
     // These are all the locales you want to support in
     // your application
-    locales: ['en-US', 'fr', 'nl-NL'],
+    locales: ["en-US", "fr", "nl-NL"],
     // This is the default locale you want to be used when visiting
     // a non-locale prefixed path e.g. `/hello`
-    defaultLocale: 'en-US',
+    defaultLocale: "en-US",
     // This is a list of locale domains and the default locale they
     // should handle (these are only required when setting up domain routing)
     // Note: subdomains must be included in the domain value to be matched e.g. "fr.example.com".
     domains: [
       {
-        domain: 'example.com',
-        defaultLocale: 'en-US',
+        domain: "example.com",
+        defaultLocale: "en-US",
       },
       {
-        domain: 'example.ru',
-        defaultLocale: 'ru-RU',
+        domain: "example.ru",
+        defaultLocale: "ru-RU",
       },
       {
-        domain: 'example.kg',
-        defaultLocale: 'kg',
+        domain: "example.kg",
+        defaultLocale: "kg",
         // an optional http field can also be used to test
         // locale domains locally with http instead of https
         http: true,
       },
     ],
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
   },
-}
+};
