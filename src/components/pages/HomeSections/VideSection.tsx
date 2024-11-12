c3cec4cfc2179deb833ce070b857f5e51fe4f508
Kuronbaev <amirkuronbaev07@gmail.com>
2024-11-12 15:38:54 +0600

    add modalwindow youtube

--- src/components/pages/HomeSections/VideSection.tsx
+++ src/components/pages/HomeSections/VideSection.tsx
@@ -1,51 +1,65 @@
-import { useTranslation } from 'react-i18next';
-import scss from './VideoSection.module.scss';
+import play from '@/assets/img/YouTubePlay.png'
+import VideoModals from '@/components/ui/VideoModals/VideoModals'
+import Image from 'next/image'
+import { useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import scss from './VideoSection.module.scss'
 
 const VideoSection = () => {
-  const { t } = useTranslation();
+	const { t } = useTranslation()
+	const [isModalOpen, setIsModalOpen] = useState(false)
+	const [selectedVideoId, setSelectedVideoId] = useState('')
 
-  return (
-    <section className={scss.VideoSection}>
-      <div className='container'>
-        <div className={scss.content}>
-          <h1>{t('videoTestimonialsTitle')}</h1>
-          <div className={scss.videoWrapper}>
-            <div className={scss.videoScroll}>
-              <iframe
-                width='570px'
-                height='340px'
-                src='https://www.youtube.com/embed/lWz-46NPqdk?si=yyDXQy8y1xDWXbIc'
-                title='YouTube video player'
-                allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-								id='1'
-              ></iframe>
-              <iframe
-                width='570px'
-                height='340px'
-                src='https://www.youtube.com/embed/lWz-46NPqdk?si=yyDXQy8y1xDWXbIc'
-                title='YouTube video player'
-                allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-              ></iframe>
-              <iframe
-                width='570px'
-                height='340px'
-                src='https://www.youtube.com/embed/lWz-46NPqdk?si=yyDXQy8y1xDWXbIc'
-                title='YouTube video player'
-                allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-              ></iframe>
-              <iframe
-                width='570px'
-                height='340px'
-                src='https://www.youtube.com/embed/lWz-46NPqdk?si=yyDXQy8y1xDWXbIc'
-                title='YouTube video player'
-                allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-              ></iframe>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-};
+	// Открытие модалки с видео
+	const openModal = videoId => {
+		setSelectedVideoId(videoId)
+		setIsModalOpen(true)
+	}
 
-export default VideoSection;
+	// Закрытие модалки
+	const closeModal = () => {
+		setIsModalOpen(false)
+	}
+
+	return (
+		<section className={scss.VideoSection}>
+			<div className='container'>
+				<div className={scss.content}>
+					<h1>{t('videoTestimonialsTitle')}</h1>
+					<div className={scss.videoWrapper}>
+						<div className={scss.videoScroll}>
+							{/* Список видео */}
+							{['lWz-46NPqdk', 'lWz-46NPqdk', 'lWz-46NPqdk', 'lWz-46NPqdk'].map(
+								(videoId, index) => (
+									<div
+										key={index}
+										className={scss.videoItem}
+										onClick={() => openModal(videoId)}
+									>
+										{/* Превью видео (например, с YouTube) */}
+										<img
+											src={`https://img.youtube.com/vi/${videoId}/0.jpg`}
+											alt={`Video thumbnail ${index + 1}`}
+											className={scss.videoThumbnail}
+										/>
+										{/* Можно добавить иконку play */}
+										<div className={scss.playButton}>
+											<Image src={play} alt='play' />
+										</div>
+									</div>
+								)
+							)}
+						</div>
+					</div>
+				</div>
+			</div>
+
+			{/* Модалка */}
+			{isModalOpen && (
+				<VideoModals videoId={selectedVideoId} onClose={closeModal} />
+			)}
+		</section>
+	)
+}
+
+export default VideoSection
