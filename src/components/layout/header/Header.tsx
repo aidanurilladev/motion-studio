95c959f98946da09e237135235debaa8954698c6
aidanurilladev <aidanurillaa@gmail.com>
2024-11-14 00:00:06 +0600

    comm

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,66 +1,88 @@
-'use client'
-import Image from 'next/image'
-import Link from 'next/link'
-import { FC } from 'react'
-import { useTranslation } from 'react-i18next'
-import Vector from '../../../assets/img/Vector.svg'
-import BurgerMenu from './BurgerMenu/BurgerMenu'
-import scss from './Header.module.scss'
+"use client";
 
-const Header: FC = () => {
-	const { t, i18n } = useTranslation()
+import { useEffect, useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import Link from "next/link";
+import { useTranslation } from "react-i18next";
+import Image from "next/image";
+import Vector from "../../../assets/img/Vector.svg";
+import BurgerMenu from "./BurgerMenu/BurgerMenu";
+import scss from "./Header.module.scss";
 
-	const handleLanguageChange = (
-		event: React.ChangeEvent<HTMLSelectElement>
-	) => {
-		i18n.changeLanguage(event.target.value)
-	}
+const Header = () => {
+  const { t, i18n } = useTranslation();
+  const [isMounted, setIsMounted] = useState(false);
+	const pathname = usePathname()
 
-	return (
-		<header className={scss.Header}>
-			<div className='container'>
-				<div className={scss.content}>
-					<div className={scss.Logo}>
-						<Link href='/'>
-							<Image src={Vector} alt='Logo' width={50} height={50} />
-						</Link>
-					</div>
-					<div className={scss.nav}>
-						<Link href='/'>{t('home')}</Link>
-						<Link href='/about'>{t('about_us')}</Link>
-						<Link href='/study'>{t('study_abroad')}</Link>
-						<Link href='#contacts'>{t('contacts')}</Link>
-					</div>
-					<div className={scss.headerInput}>
-						<div className={scss.searchInput}>
-							<input type='text' placeholder={t('search_placeholder')} />
-							<svg
-								xmlns='http://www.w3.org/2000/svg'
-								width='16'
-								height='16'
-								fill='currentColor'
-								viewBox='0 0 16 16'
-								className={scss.searchIcon}
-							>
-								<path d='M11.742 10.344a6.5 6.5 0 1 0-1.398 1.398l3.85 3.85a1 1 0 0 0 1.415-1.415l-3.85-3.85zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z' />
-							</svg>
-						</div>
-						<select
-							onChange={handleLanguageChange}
-							defaultValue={i18n.language}
-						>
-							<option value='en'>EN</option>
-							<option value='ru'>RU</option>
-							<option value='kg'>KG</option>
-						</select>
-					</div>
-					<div className={scss.burgerMenu}>
-						<BurgerMenu />
-					</div>
-				</div>
-			</div>
-		</header>
-	)
-}
 
-export default Header
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setIsMounted(true);
+    }
+  }, []);
+
+  const router = isMounted ? useRouter() : null;
+
+  const handleLanguageChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    i18n.changeLanguage(event.target.value);
+  };
+
+  if (!isMounted) return null; 
+
+  return (
+    <header className={scss.Header}>
+      <div className="container">
+        <div className={scss.content}>
+          <div className={scss.Logo}>
+            <Link href="/">
+              <Image src={Vector} alt="Logo" width={50} height={50} />
+            </Link>
+          </div>
+          <div className={scss.nav}>
+            <Link
+              href="/"
+              className={pathname === "/" ? scss.active : ""}
+            >
+              {t("home")}
+            </Link>
+            <Link
+              href="/about"
+              className={pathname === "/about" ? scss.active : ""}
+            >
+              {t("about_us")}
+            </Link>
+            <Link
+              href="/study"
+              className={pathname === "/study" ? scss.active : ""}
+            >
+              {t("study_abroad")}
+            </Link>
+            <Link
+              href="/#contacts"
+              className={pathname === "/#contacts" ? scss.active : ""}
+            >
+              {t("contacts")}
+            </Link>
+          </div>
+          <div className={scss.headerInput}>
+            <select
+              onChange={handleLanguageChange}
+              defaultValue={i18n.language}
+            >
+              <option value="en">EN</option>
+              <option value="ru">RU</option>
+              <option value="kg">KG</option>
+            </select>
+          </div>
+          <div className={scss.burgerMenu}>
+            <BurgerMenu />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
